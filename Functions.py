d278a65f80208dee4cce4a2665db8de4dad03a2c
9AD589B1 <kocha017@gmail.com>
2018-10-02 20:43:36 -0500

    Changes

--- Functions.py
+++ Functions.py
@@ -36,3 +36,22 @@ def generate_evens():
 generate_evens()
 
 print(2**8)
+
+def yell(string):
+    return string.upper() + "!"
+
+def speak(animal="dog"):
+    if animal == "pig":
+        return "oink"
+    elif animal == "duck":
+        return "quack"
+    elif animal == "cat":
+        return "meow"
+    elif animal == "dog":
+        return "woof"
+    else:
+        return "?"
+
+def speak(animal='dog'):
+    noises = {'pig':'oink', 'duck':'quack', 'cat':'meow', 'dog':'woof'}
+    return noises.get(animal, '?')
