2058ed0909518c8abe4736dd67dee3c811448f7e
9AD589B1 <kocha017@gmail.com>
2018-10-07 16:10:46 -0500

    Changes

--- Functions.py
+++ Functions.py
@@ -55,3 +55,38 @@ def speak(animal="dog"):
 def speak(animal='dog'):
     noises = {'pig':'oink', 'duck':'quack', 'cat':'meow', 'dog':'woof'}
     return noises.get(animal, '?')
+
+def product(num1, num2):
+    return num1 * num2
+
+product(2,2) # 4
+product(2,-2) # -4
+
+def return_day(day):
+    day_of_week = {1:'Sunday', 2:'Monday', 3:'Tuesday', 4:'Wednesday', 5:'Thursday', 6:'Friday', 7:'Saturday'}
+    return day_of_week.get(day, 'None')
+
+return_day(1) # "Sunday"
+return_day(2) # "Monday"
+return_day(3) # "Tuesday"
+return_day(4) # "Wednesday"
+return_day(5) # "Thursday"
+return_day(6) # "Friday"
+return_day(7) # "Saturday"
+return_day(41) # None
+
+def last_element(l):
+    if len(l) > 0:
+        return l[-1]
+    return None
+    
+last_element([1,2,3])
+
+def number_compare(num1, num2):
+    if num1 != num2:
+        if num1 > num2:
+            return "First is greater"
+        return "Second is greater"
+    return "Numbers are equal"
+
+number_compare(2,1)
