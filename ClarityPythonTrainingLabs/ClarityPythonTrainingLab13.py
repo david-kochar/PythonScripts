40a6375fb6188e1d639442c3e92425505d09ac8b
9AD589B1 <kocha017@gmail.com>
2018-11-17 19:08:08 -0600

    Changes

---
+++ ClarityPythonTrainingLabs/ClarityPythonTrainingLab13.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 17 18:27:47 2018
+
+Read in the contents of the file my_hidden.txt and replace all occurrences of 
+the word "hidden" with your favorite subject in school. Write the results to a 
+new file called my_favorite_subject.txt.
+
+@author: DK
+"""
+
+txt_file = open("myhiddentext.txt",'r')
+file_data = txt_file.read()
+txt_file.close()
+
+new_txt_file = file_data.replace("hidden", "math")
+txt_file = open("myhiddentext.txt",'w')
+txt_file.write(new_txt_file)
+txt_file.close()
