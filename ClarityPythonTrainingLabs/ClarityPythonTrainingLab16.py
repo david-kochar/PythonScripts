8f40cca9f8824be0deb365edbe58006e60f11586
9AD589B1 <kocha017@gmail.com>
2018-11-20 11:51:55 -0600

    Update ClarityPythonTrainingLab16.py

--- ClarityPythonTrainingLabs/ClarityPythonTrainingLab16.py
+++ ClarityPythonTrainingLabs/ClarityPythonTrainingLab16.py
@@ -3,5 +3,44 @@
 Created on Tue Nov 20 08:36:23 2018
 
 @author: DK
-"""
 
+What is the regex pattern to match the following:
+• Email (such as name@email.com)
+• Email with .com, .edu or .gov address
+• Email with no numbers allowed in
+  mailbox name
+
+"""
+import re
+
+#What is the regex pattern to match the following:
+
+#Email (such as name@email.com)
+def valid_email(email):
+    if not re.match("[^@]+@[^@]+\.[^@]+", email):
+        return f"'{email}' is not a valid email"
+    else:
+        return f"'{email}' is a valid email"
+    
+valid_email("me@me.com")
+valid_email("foo")
+    
+#Email with .com, .edu or .gov address
+def email_domain_whitelist(email):
+    if not re.match("[^@]+@[^@]+\.(^@|com|gov|edu)+", email):
+        return f"'{email}' does not have a whitelisted domain"
+    else:
+        return f"'{email}' has a whitelisted domain"
+
+email_domain_whitelist("me@me.com")    
+email_domain_whitelist("me@me.net")
+
+#Email with no numbers allowed in mailbox name
+def email_mailbox_nonumeric(email):
+    if not re.match("[^@0-9]+@[^@]+\.[^@]+", email):
+        return f"'{email}' is not a vaild email. Numbers in the mailbox are not allowed"
+    else:
+        return f"'{email}' is a valid email"
+
+email_mailbox_nonumeric("me@me.com")
+email_mailbox_nonumeric("me2@me.com")
