66febd74d064fb1af92f78bab8fdef432648471c
9AD589B1 <kocha017@gmail.com>
2018-11-24 15:05:39 -0600

    Changes

--- ClarityPythonTrainingLabs/ClarityPythonTrainingLab11_v2.py
+++ ClarityPythonTrainingLabs/ClarityPythonTrainingLab11_v2.py
@@ -13,16 +13,22 @@ numbers under 50 and over 20, by utilizing the sets from steps (1) and (2).
 
 @author: DK
 """
+
 def find_primes(num1, num2):
     
     lst = []
     
     for x in range(num1, num2):
+        factor_counter = 0
         for y in range(1, num2, 1): #for y in range(1, num2 - (num2 - (x + 1))):
             if x % y == 0:
-                lst.append(x)
-    return set([i for i in lst if lst.count(i) <= 2])
- 
+                factor_counter += 1
+            if factor_counter > 2:
+                break
+        if factor_counter == 2:
+            lst.append(x)
+    return set(lst)
+
 exam_grades_under_50 = find_primes(2,50)
 exam_grades_under_20 = exam_grades_under_50.difference(find_primes(21,50))
 exam_grades_under_50_over_20 = exam_grades_under_50.difference(exam_grades_under_20)
@@ -31,3 +37,18 @@ print(f"Exam grades under 50 are {exam_grades_under_50}")
 print(f"Exam grades under 20 are {exam_grades_under_20}")
 print(f"Exam grades under 50 and over 20 are {exam_grades_under_50_over_20}")
 
+"""
+lst = []
+
+for x in range(2, 11):
+    factor_counter = 0
+    for y in range(1, 11, 1): #for y in range(1, num2 - (num2 - (x + 1))):
+        if x % y == 0:
+            factor_counter += 1
+        if factor_counter > 2:
+            break
+    if factor_counter == 2:
+        lst.append(x)
+        
+print(lst)
+"""
--- ClarityPythonTrainingLabs/nestedtest.py
+++ ClarityPythonTrainingLabs/nestedtest.py
@@ -1,21 +1,8 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Nov 19 14:37:35 2018
+Created on Fri Nov 23 12:26:06 2018
 
 @author: DK
-
-Given the following JSON string, parse it and write the data records to a csv 
-file called fruits.csv.
-
-{
-    "total": 3,
-    "data": [
-        { "id": 1, "name": "grapes", "quantity": 100, "cost": 1.01 },
-        { "id": 2, "name": "strawberries", "quantity": 50, "cost": 3.71 },
-        { "id": 3, "name": "lemon", "quantity": 10, "cost": 2.17 }
-    ]
-}
-
 """
 
 import json
@@ -31,11 +18,15 @@ json_string_fruits = """
 }
 """
 fruits_dict = json.loads(json_string_fruits) #convert json to dict
-#[y for x in non_flat for y in x]
+
+"""
 with open('fruits.csv', 'w') as f: #create csv
     for record in fruits_dict["data"]: #access "data" key
         #for each nested dict in data, get their values as str, separate by 
         #comma, and then add new line
         f.write("," .join([str(i) for i in list(record.values())]) + "\n")
+"""
+with open('fruits.csv', 'w') as f: #create csv
+    f.write("," .join([str(i) for record in fruits_dict["data"] for i in list(record.values())]))
         
 f.close()
