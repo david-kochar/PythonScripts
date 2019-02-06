dc2fff35c552a9feac1555fb0e649ebdba629aa1
9AD589B1 <kocha017@gmail.com>
2019-02-06 17:08:01 -0600

    Changes

--- ZillowSearchScraper.py
+++ ZillowSearchScraper.py
@@ -4,7 +4,7 @@ Created on Sat Feb  2 10:00:33 2019
 
 @author: DK
 
-Webscraper for Zillow listings
+This is a webscraper app for Zillow listings, which exports to csv
 """
 
 import os
@@ -21,72 +21,59 @@ page    = requests.get(url, headers = headers)
 tree    = html.fromstring(page.content)
 
 """
-pagetext = page.text
-tree_output = open("ZillowSearchHTML.txt",'w')
-tree_output.write(pagetext)
-tree_output.close()
-"""
-
-"""
 Parse listing data. Note that "specs" will need to be further parsed as it
 contains nested dicts of property details
 """
-
-#addresses = [str(i) for i in tree.xpath('//span[@class="zsg-photo-card-address"]/text()')]
-#prices = [str(i) for i in tree.xpath('//span[@class="zsg-photo-card-price"]/text()')]
 specs = [str(i).replace("<!--", "").replace("-->", "").replace("\\", "").replace("false", "False").replace("true", "True")
         for i in tree.xpath('//li//div//comment()')]
 
 specs = [ast.literal_eval(i) for i in specs] #remove quotes to allow iteration
 
 """
-#Individual variables
+#Individual variables example
 
 zpid = [i["homeInfo"]["zpid"] for i in specs]
-streetAddress = [i["homeInfo"]["streetAddress"] for i in specs]
-zipcode = [i["homeInfo"]["zipcode"] for i in specs]
-city = [i["homeInfo"]["city"] for i in specs]
-state = [i["homeInfo"]["state"] for i in specs]
-price = [i["homeInfo"]["price"] for i in specs]
-dateSold = [i["homeInfo"]["dateSold"] for i in specs]
-bathrooms = [i["homeInfo"]["bathrooms"] for i in specs]
-bedrooms = [i["homeInfo"]["bedrooms"] for i in specs]
-livingArea = [i["homeInfo"]["livingArea"] for i in specs]
-yearBuilt = [i["homeInfo"]["yearBuilt"] for i in specs]
-lotSize = [i["homeInfo"]["lotSize"] for i in specs]
-homeType = [i["homeInfo"]["homeType"] for i in specs]
-homeStatus = [i["homeInfo"]["homeStatus"] for i in specs]
-daysOnZillow = [i["homeInfo"]["daysOnZillow"] for i in specs]
-brokerId = [i["homeInfo"]["brokerId"] for i in specs]
-contactPhone = [i["homeInfo"]["contactPhone"] for i in specs]
-zestimate = [i["homeInfo"]["zestimate"] for i in specs]
-isPreforeclosureAuction = [i["isPreforeclosureAuction"] for i in specs]
-homeStatusForHDP = [i["homeStatusForHDP"] for i in specs]
-priceForHDP = [i["priceForHDP"] for i in specs]
-festimate = [i["festimate"] for i in specs]
-isListingOwnedByCurrentSignedInAgent = [i["isListingOwnedByCurrentSignedInAgent"] for i in specs]
-timeOnZillow = [i["timeOnZillow"] for i in specs]
-isListingClaimedByCurrentSignedInUser = [i["isListingClaimedByCurrentSignedInUser"] for i in specs]
-contactPhoneExtension = [i["contactPhoneExtension"] for i in specs]
-lotId = [i["lotId"] for i in specs]
-lotId64 = [i["lotId64"] for i in specs]
-isNonOwnerOccupied = [i["isNonOwnerOccupied"] for i in specs]
-isPremierBuilder = [i["isPremierBuilder"] for i in specs]
 isZillowOwned = [i["isZillowOwned"] for i in specs]
 """
 
 #Create sets of dict keys from specs
-specs_keys          = set().union(*(d.keys() for d in specs))
-specs_homeinfo_keys = ["zpid", "streetAddress", "zipcode", "city", "state",        
-"price","dateSold", "bathrooms", "bedrooms", "livingArea", "yearBuilt", 
-"lotSize", "homeType", "homeStatus", "daysOnZillow", "brokerId", "contactPhone", 
-"zestimate"] #set().union(*(d["homeInfo"].keys() for d in specs))
+#specs_keys          = set().union(*(d.keys() for d in specs))
+specs_homeinfo_keys = ["zpid", 
+                       "streetAddress", 
+                       "zipcode", 
+                       "city", 
+                       "state",
+                       "price",
+                       "dateSold", 
+                       "bathrooms", 
+                       "bedrooms", 
+                       "livingArea", 
+                       "yearBuilt", 
+                       "lotSize", 
+                       "homeType", 
+                       "homeStatus", 
+                       "daysOnZillow", 
+                       "brokerId", 
+                       "contactPhone", 
+                       "zestimate" 
+                      ] #set().union(*(d["homeInfo"].keys() for d in specs))
 
 #Create a nested dict of all listings
 specs_dict = {}
 
+#Generate nested dicts where each dict represents a listing record
 for i in specs:
     for k in i["homeInfo"]:
         specs_dict[specs.index(i)] = {k: i["homeInfo"].get(k, None) for k in specs_homeinfo_keys}
-        
 
+#Export csv of listings
+with open('ZillowReport.csv', 'w') as f: #create csv
+    
+    #write header row
+    f.write("," .join([i for i in specs_homeinfo_keys]) + "\n")
+    
+    #write row for each listing dict.values
+    for record in specs_dict.values(): 
+        f.write("," .join([str(i) for i in list(record.values())]) + "\n")
+        
+f.close()
