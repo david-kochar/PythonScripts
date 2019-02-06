6e32118b4a789db9126fb22efce6f6b5e3f9eff3
9AD589B1 <kocha017@gmail.com>
2019-02-06 12:34:08 -0600

    Update ZillowSearchScraper.py

--- ZillowSearchScraper.py
+++ ZillowSearchScraper.py
@@ -16,9 +16,9 @@ from lxml import html
 import ast
 
 headers = {'user-agent': 'ListingsScraper/0.0.1'}
-url = "https://www.zillow.com/homes/for_sale/Minneapolis-MN/pmf,pf_pt/condo_type/5983_rid/globalrelevanceex_sort/45.107211,-92.92923,44.815941,-93.421212_rect/11_zm/"
-page = requests.get(url, headers = headers)
-tree = html.fromstring(page.content)
+url     = "https://www.zillow.com/homes/for_sale/Minneapolis-MN/pmf,pf_pt/condo_type/5983_rid/globalrelevanceex_sort/45.107211,-92.92923,44.815941,-93.421212_rect/11_zm/"
+page    = requests.get(url, headers = headers)
+tree    = html.fromstring(page.content)
 
 """
 pagetext = page.text
@@ -29,7 +29,7 @@ tree_output.close()
 
 """
 Parse listing data. Note that "specs" will need to be further parsed as it
-contains dictionaries
+contains nested dicts of property details
 """
 
 #addresses = [str(i) for i in tree.xpath('//span[@class="zsg-photo-card-address"]/text()')]
@@ -37,11 +37,60 @@ contains dictionaries
 specs = [str(i).replace("<!--", "").replace("-->", "").replace("\\", "").replace("false", "False").replace("true", "True")
         for i in tree.xpath('//li//div//comment()')]
 
-specs = [ast.literal_eval(i) for i in specs]
+specs = [ast.literal_eval(i) for i in specs] #remove quotes to allow iteration
 
-addresses = [i["homeInfo"]["streetAddress"] for i in specs]
+"""
+zp_ids           = [i["homeInfo"]["zpid"] for i in specs]
+street_addresses = [i["homeInfo"]["streetAddress"] for i in specs]
+cities           = [i["homeInfo"]["city"] for i in specs]
+zip_codes        = [i["homeInfo"]["zipcode"] for i in specs]
+states           = [i["homeInfo"]["state"] for i in specs]
+prices           = [i["homeInfo"]["price"] for i in specs]
+bedrooms         = [i["homeInfo"]["bedrooms"] for i in specs]
+bathrooms        = [i["homeInfo"]["bathrooms"] for i in specs]
+living_areas     = [i["homeInfo"]["livingArea"] for i in specs]
+years_built      = [i["homeInfo"]["yearBuilt"] for i in specs]
+home_types       = [i["homeInfo"]["homeType"] for i in specs]
+"""
 
-addresses
+#Create sets of dict keys from specs
+specs_keys          = set().union(*(d.keys() for d in specs))
+specs_homeinfo_keys = set().union(*(d["homeInfo"].keys() for d in specs))
+
+specs_homeinfo_keys
+
+
+zpid = [i["homeInfo"]["zpid"] for i in specs]
+streetAddress = [i["homeInfo"]["streetAddress"] for i in specs]
+zipcode = [i["homeInfo"]["zipcode"] for i in specs]
+city = [i["homeInfo"]["city"] for i in specs]
+state = [i["homeInfo"]["state"] for i in specs]
+price = [i["homeInfo"]["price"] for i in specs]
+dateSold = [i["homeInfo"]["dateSold"] for i in specs]
+bathrooms = [i["homeInfo"]["bathrooms"] for i in specs]
+bedrooms = [i["homeInfo"]["bedrooms"] for i in specs]
+livingArea = [i["homeInfo"]["livingArea"] for i in specs]
+yearBuilt = [i["homeInfo"]["yearBuilt"] for i in specs]
+lotSize = [i["homeInfo"]["lotSize"] for i in specs]
+homeType = [i["homeInfo"]["homeType"] for i in specs]
+homeStatus = [i["homeInfo"]["homeStatus"] for i in specs]
+daysOnZillow = [i["homeInfo"]["daysOnZillow"] for i in specs]
+brokerId = [i["homeInfo"]["brokerId"] for i in specs]
+contactPhone = [i["homeInfo"]["contactPhone"] for i in specs]
+zestimate = [i["homeInfo"]["zestimate"] for i in specs]
+isPreforeclosureAuction = [i["isPreforeclosureAuction"] for i in specs]
+homeStatusForHDP = [i["homeStatusForHDP"] for i in specs]
+priceForHDP = [i["priceForHDP"] for i in specs]
+festimate = [i["festimate"] for i in specs]
+isListingOwnedByCurrentSignedInAgent = [i["isListingOwnedByCurrentSignedInAgent"] for i in specs]
+timeOnZillow = [i["timeOnZillow"] for i in specs]
+isListingClaimedByCurrentSignedInUser = [i["isListingClaimedByCurrentSignedInUser"] for i in specs]
+contactPhoneExtension = [i["contactPhoneExtension"] for i in specs]
+lotId = [i["lotId"] for i in specs]
+lotId64 = [i["lotId64"] for i in specs]
+isNonOwnerOccupied = [i["isNonOwnerOccupied"] for i in specs]
+isPremierBuilder = [i["isPremierBuilder"] for i in specs]
+isZillowOwned = [i["isZillowOwned"] for i in specs]
 
 
 t = [{  
