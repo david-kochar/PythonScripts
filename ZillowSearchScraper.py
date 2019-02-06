44bd48de01fce36fd57db199a8cc9826442e931d
9AD589B1 <kocha017@gmail.com>
2019-02-06 08:51:20 -0600

    Changes

--- ZillowSearchScraper.py
+++ ZillowSearchScraper.py
@@ -3,24 +3,27 @@
 Created on Sat Feb  2 10:00:33 2019
 
 @author: DK
+
+Webscraper for Zillow listings
 """
+
 import os
 os.chdir("C:\\Users\DK\Documents\\GitHub\\PythonScripts")
-
-from lxml import html
 import requests
+from lxml import html
 
-url = "https://www.zillow.com/homes/for_sale/pmf,pf_pt/condo,townhouse_type/1-_beds/150000-250000_price/592-987_mp/mostrecentchange_sort/45.023067,-93.209639,44.950281,-93.332634_rect/13_zm/X1.dash.SS.dash.16fg02arlqet_4n5nu_sse/ec9b9592c8X1-CRylkhrb73r81q_vpzyi_crid/"
-page = requests.get(url)
-pagetext = page.text
-sourcecode = page.content #get string of source code from response
+headers = {'user-agent': 'Minneapolis-Condos/0.0.1'}
+url = "https://www.zillow.com/homes/for_sale/Minneapolis-MN/pmf,pf_pt/condo_type/5983_rid/globalrelevanceex_sort/45.107211,-92.92923,44.815941,-93.421212_rect/11_zm/"
+page = requests.get(url, headers = headers)
 tree = html.fromstring(page.content)
 
-type(sourcecode)
-
+"""
+pagetext = page.text
 tree_output = open("ZillowSearchHTML.txt",'w')
 tree_output.write(pagetext)
 tree_output.close()
+"""
 
-import os
-print(os.getcwd())
+addresses = [str(i) for i in tree.xpath('//span[@class="zsg-photo-card-address"]/text()')]
+prices = [str(i) for i in tree.xpath('//span[@class="zsg-photo-card-price"]/text()')]
+specs = [str(i).replace("<!--", "").replace("-->", "") for i in tree.xpath('//li//div//comment()')]
