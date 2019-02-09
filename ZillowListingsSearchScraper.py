e5db7f1c88f0852e141d2fbbe6d825a2dcc851ec
9AD589B1 <kocha017@gmail.com>
2019-02-09 13:36:30 -0600

    Update ZillowListingsSearchScraper.py

--- ZillowListingsSearchScraper.py
+++ ZillowListingsSearchScraper.py
@@ -3,32 +3,35 @@
 Created on Thu Feb  7 20:33:07 2019
 
 @author: dkochar
+
+This is a webscraper app for Zillow listings, which exports to csv
+
 """
 
 import requests
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
 from lxml import html
-import ast
+#import ast
 
 session = requests.Session()
-retry = Retry(connect=3, backoff_factor=0.5)
+retry   = Retry(connect=3, backoff_factor=0.5)
 adapter = HTTPAdapter(max_retries=retry)
 session.mount('http://', adapter)
 session.mount('https://', adapter)
 
-headers = {'user-agent': 'ListingsScraper/0.0.1'}
-url = "https://www.zillow.com/homes/for_sale/Minneapolis-MN/5983_rid/globalrelevanceex_sort/45.116055,-93.015404,44.825073,-93.507386_rect/11_zm/"
-base_url = url
-urls = [base_url]
-specs = []
-specs_dict = {} #create empty dict
+headers    = {'user-agent': 'ListingsScraper/0.0.1'} #create headers for legimate requests
+url        = "https://www.zillow.com/homes/for_sale/Minneapolis-MN/5983_rid/globalrelevanceex_sort/45.116055,-93.015404,44.825073,-93.507386_rect/11_zm/"
+base_url   = url
+urls       = [base_url] #seed urls list with base url
+specs      = [] #create empty list for listing dicts
+specs_dict = {} #create empty dict for nesting listing dicts
 
-path_counter = 2
+path_counter = 2 #succesive listing pages start at 2
 while True:
     url = base_url + str(path_counter) + "_p/"
     response = requests.head(url, headers = headers)
-    if response.status_code != 200:
+    if response.status_code != 200: #build url list for all valid listing pages
         break
     urls.append(url)
     path_counter += 1
@@ -36,12 +39,23 @@ while True:
 for url in urls:
     page  = session.get(url, headers = headers)
     tree  = html.fromstring(page.content)
-    specs.append( [str(i).replace("<!--", "").replace("-->", "").replace("\\", "").replace("false", "False").replace("true", "True")
-            for i in tree.xpath('//li//div//comment()')])
-    
+    #relevant listing info is embedded in comments, and need to pre-scrub html to coerce into dicts
+    specs.append( [str(i).replace('<!--', '').replace('-->', '').replace('\\', '').replace('false', 'False').replace('true', 'True').replace('null', '"' + '' + '"' )
+            for i in tree.xpath('//li//div//comment()')] )
+
+"""
+with open('specs_dump.txt', 'w') as f:
+    for item in specs:
+        f.write("%s\n" % item)
+f.close()
+"""
+
 #Parse listing data. Note that "specs" will need to be further parsed as it
 #contains nested dicts of property details
-specs = [ast.literal_eval(i) for i in specs] #remove quotes to allow iteration
+
+specs = [ eval(i) for lst in specs for i in lst ] #remove quotes to allow iteration
+
+#create list of relevant dict keys for nested "homeInfo" dicts
 specs_homeinfo_keys = ["zpid", 
                        "streetAddress", 
                        "zipcode", 
@@ -62,13 +76,12 @@ specs_homeinfo_keys = ["zpid",
                        "zestimate" 
                       ] #set().union(*(d["homeInfo"].keys() for d in specs))
 
-#Create a nested dict of all listings
-
 #Generate nested dicts where each dict represents a listing record    
 for i in specs:
     for k in i["homeInfo"]:
-        specs_dict[specs.index(i)] = {k: i["homeInfo"].get(k, None) for k in specs_homeinfo_keys} #Export csv of listings
+        specs_dict[specs.index(i)] = {k: i["homeInfo"].get(k, '') for k in specs_homeinfo_keys} 
 
+#Export csv of listings
 with open('ZillowReport.csv', 'w') as f: #create csv
     
     #write header row
