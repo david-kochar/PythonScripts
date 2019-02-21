c981dbae048fd37df8eecbce2434fbe3becf5b55
9AD589B1 <kocha017@gmail.com>
2019-02-20 22:03:27 -0600

    Updates

--- ZillowListingsSearchScraper.py
+++ ZillowListingsSearchScraper.py
@@ -12,13 +12,15 @@ import requests
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
 from lxml import html
+from datetime import date
 
 session = requests.Session()
-retry   = Retry(connect=3, backoff_factor=0.5) #used for request to delay to avoid blacklisting
-adapter = HTTPAdapter(max_retries=retry)
+retry   = Retry(connect = 3, backoff_factor = 0.5) #used for request to delay to avoid blacklisting
+adapter = HTTPAdapter(max_retries = retry)
 session.mount('http://', adapter)
 session.mount('https://', adapter)
 
+today      = str(date.today())
 headers    = {'user-agent': 'ListingsScraper/0.0.1'} #create headers for legimate requests
 state      = input ( "Enter a state two-letter abbreviation: " )
 city       = input ( "Enter a city: " )
@@ -30,7 +32,7 @@ specs_dict = {} #create empty dict for nesting listing dicts
 
 path_counter = 2 #succesive listing pages start at 2
 while True:
-    url = base_url + str(path_counter) + "_p/" #zillow.com url structure appends page count and _p/
+    url      = base_url + str(path_counter) + "_p/" #zillow.com url structure appends page count and _p/
     response = requests.head(url, headers = headers)
     if response.status_code != 200: #build url list for all valid listing pages
         break
@@ -77,7 +79,7 @@ for i in specs:
         specs_dict[specs.index(i)] = {k: i["homeInfo"].get(k, '') for k in specs_homeinfo_keys} 
 
 #Export csv of listings
-with open ( 'ZillowReport.csv', 'w' ) as f: #create csv
+with open ( 'ZillowListingsfor' + city + state + '_' + today + '.csv', 'w' ) as f: #create csv
     
     #write header row
     f.write ("," .join([i for i in specs_homeinfo_keys]) + "\n")
@@ -88,4 +90,4 @@ with open ( 'ZillowReport.csv', 'w' ) as f: #create csv
          
 f.close()
 
-print(f"Zillow For-Sale Listings extract complete for {city}, {state}")
+print ( f"Zillow For-Sale listings extract complete for {city}, {state}" )
