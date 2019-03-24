f6b4d78fd0c02d6370c9ea75f575d8e979f04e11
9AD589B1 <kocha017@gmail.com>
2019-03-24 10:18:59 -0500

    Update ZillowListingsSearchScraper.py

--- ZillowListingsSearchScraper.py
+++ ZillowListingsSearchScraper.py
@@ -14,37 +14,45 @@ from requests.packages.urllib3.util.retry import Retry
 from lxml import html
 from datetime import date
 
-session = requests.Session()
-retry   = Retry(connect = 3, backoff_factor = 0.5) #used for request to delay to avoid blacklisting
-adapter = HTTPAdapter(max_retries = retry)
-session.mount('http://', adapter)
-session.mount('https://', adapter)
-
-today      = str(date.today())
-headers    = {'user-agent': 'ListingsScraper/0.0.1'} #create headers for legimate requests
-state      = input ( "Enter a state two-letter abbreviation: " )
-city       = input ( "Enter a city: " )
-url        = "https://www.zillow.com/homes/for_sale/" + city + "-" + state + "/11_zm/" #base url for listings
-base_url   = url
-urls       = [base_url] #seed urls list with base url
-specs      = [] #create empty list for listing dicts
-specs_dict = {} #create empty dict for nesting listing dicts
+today            = str(date.today())
+headers          = {'user-agent': 'ListingsScraper/0.0.1'} #create headers for legimate requests
+for_sale_or_rent = input ( "Enter for_sale or for_rent: " )
+state            = input ( "Enter a state two-letter abbreviation: " )
+city             = input ( "Enter a city: " )
+url              = "https://www.zillow.com/homes/" + for_sale_or_rent + "/" + city + "-" + state + "/11_zm/" #base url for listings
+base_url         = url
+urls             = [base_url] #seed urls list with base url
+specs            = [] #create empty list for listing dicts
+specs_dict       = {} #create empty dict for nesting listing dicts
 
 path_counter = 2 #succesive listing pages start at 2
 while True:
+    session = requests.Session()
+    retry   = Retry(connect = 3, backoff_factor = 0.5) #used for request to delay to avoid blacklisting
+    adapter = HTTPAdapter(max_retries = retry)
+    session.mount('http://', adapter)
+    session.mount('https://', adapter)
     url      = base_url + str(path_counter) + "_p/" #zillow.com url structure appends page count and _p/
-    response = requests.head(url, headers = headers)
+    response = session.head(url, headers = headers)
     if response.status_code != 200: #build url list for all valid listing pages
         break
     urls.append(url)
     path_counter += 1
+    
+sub_urls = [urls[i::5] for i in range(5)]
 
-for url in urls:
-    page = session.get(url, headers = headers)
-    tree = html.fromstring(page.content)
-    #relevant listing info is embedded in comments, and need to pre-scrub html to coerce into dicts
-    specs.append( [str(i).replace('<!--', '').replace('-->', '').replace('\\', '').replace('false', 'False').replace('true', 'True').replace('null', '"' + '' + '"' )
-            for i in tree.xpath('//li//div//comment()')] )
+for sub_url in sub_urls:
+    for url in sub_url:
+        session = requests.Session()
+        retry   = Retry(connect = 3, backoff_factor = 0.5) #used for request to delay to avoid blacklisting
+        adapter = HTTPAdapter(max_retries = retry)
+        session.mount('http://', adapter)
+        session.mount('https://', adapter)
+        page = session.get(url, headers = headers)
+        tree = html.fromstring(page.content)
+        #relevant listing info is embedded in comments, and need to pre-scrub html to coerce into dicts
+        specs.append( [str(i).replace('<!--', '').replace('-->', '').replace('\\', '').replace('false', 'False').replace('true', 'True').replace('null', '"' + '' + '"' )
+                for i in tree.xpath('//li//div//comment()')] )
 
 #Parse listing data. Note that "specs" will need to be further parsed as it
 #contains nested dicts of property details
@@ -75,11 +83,14 @@ specs_homeinfo_keys = [
 
 #Generate nested dicts where each dict represents a listing record    
 for i in specs:
-    for k in i["homeInfo"]:
-        specs_dict[specs.index(i)] = {k: i["homeInfo"].get(k, '') for k in specs_homeinfo_keys} 
+    try:
+        for k in i["homeInfo"]:
+            specs_dict[specs.index(i)] = {k: i["homeInfo"].get(k, '') for k in specs_homeinfo_keys}
+    except KeyError:
+        continue
 
 #Export csv of listings
-with open ( 'ZillowListingsfor' + city + state + '_' + today + '.csv', 'w' ) as f: #create csv
+with open ( 'zillow_' + for_sale_or_rent + '_listings_' + city + '_' + state + '_' + today + '.csv', 'w' ) as f: #create csv
     
     #write header row
     f.write ("," .join([i for i in specs_homeinfo_keys]) + "\n")
@@ -90,4 +101,4 @@ with open ( 'ZillowListingsfor' + city + state + '_' + today + '.csv', 'w' ) as 
          
 f.close()
 
-print ( f"Zillow For-Sale listings extract complete for {city}, {state}" )
+print ( f"Zillow {for_sale_or_rent} listings extract complete for {city}, {state}" )
