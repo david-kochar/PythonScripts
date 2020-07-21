f01110a8ec7501684cd281b550426534ff963c5c
9AD589B1 <kocha017@gmail.com>
2020-07-21 12:16:13 -0500

    Update JokesAPI.py

--- JokesAPI.py
+++ JokesAPI.py
@@ -1,9 +1,13 @@
+import pyfiglet
 import requests
 from random import choice
 
+ascii_banner = pyfiglet.figlet_format("Dad Joke Generator")
+print(ascii_banner)
+
 url = "https://icanhazdadjoke.com/search"
 
-joke_topic = input ("Let me tell you a joke! What's your joke topic? ")
+joke_topic = input ("Let me tell you a Dad joke! What's your joke topic? ")
 
 response = requests.get(
     url, 
@@ -17,8 +21,8 @@ joke_count = len(joke_list)
 
 if joke_count > 0:
     print (f"\nI've got {joke_count} joke(s) about {joke_topic}. Here's one:\n")
-    print (choice(joke_list)) #randomly select joke
+    print (choice(joke_list) + "\n") #randomly select joke
 else:
-    print(f"Sorry, I don't have any jokes about {joke_topic}!")
+    print(f"Sorry, I don't have any jokes about {joke_topic}!\n")
 
     
