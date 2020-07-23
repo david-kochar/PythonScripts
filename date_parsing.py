cd2d02e1ef7328ed1d5202db6b77ad76314be900
9AD589B1 <kocha017@gmail.com>
2020-07-23 14:21:30 -0500

    Update date_parsing.py

--- date_parsing.py
+++ date_parsing.py
@@ -9,9 +9,9 @@ import re
 
 def parse_date(date_string):
     date_regex = re.compile(r'^(?P<d>\d{2})[\/\,\.](?P<m>\d{2})[\/\,\.](?P<y>\d{4})$')
-    matches = date_regex.search(date_string)
-    if bool(date_regex.search(date_string)):
-        return {"d":matches.group("d"), "m":matches.group("m"), "y":matches.group("y")}
+    match = date_regex.search(date_string)
+    if match:
+        return {"d":match.group("d"), "m":match.group("m"), "y":match.group("y")}
 
 parse_date("12/04/2003")
 parse_date("12,04,200312")
