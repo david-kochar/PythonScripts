1f235cd01670ec2802d478dd72c96749d699d275
9AD589B1 <kocha017@gmail.com>
2020-09-26 10:56:59 -0500

    Update test_data_generator.py

--- test_data_generator.py
+++ test_data_generator.py
@@ -1,38 +1,58 @@
 import csv
 import random as rand
 import string
+import time
 from pathlib import Path
 from random import choice
 
 
-def rand_string():
-    text = "".join(rand.choices(string.ascii_uppercase + string.digits, k=20))
+def rand_string(n):
+    n = int(n)
+    text = "".join(rand.choices(string.ascii_uppercase + string.digits, k=n))
     return text
 
 
-def rand_integer():
-    return rand.randint(1000000, 99999999)
+def rand_integer(n):
+    n = int(n)
+    return rand.randint(pow(10, n - 1), pow(10, n) - 1)
 
 
-home = str(Path.home())
-header_count = input("\nEnter number of columns to create: ")
-rows = input("Enter number of rows to create: ")
-delim = input("Enter delimiter. If tab, enter 'tab': ")
-file_name = input("Enter file name: ")
-write_path = f"{home}/{file_name}.csv"
+def write_test_data():
+    with open(write_path, mode="w", newline="", encoding="utf-8") as f:
+        writer = csv.writer(f, delimiter=delim)
+        header = ["C" + str(i + 1) for i in range(int(header_count))]
+        writer.writerow(header)
+        for i in range(int(rows)):
+            value_list = [rand_string(val_len), rand_integer(val_len)]
+            row = [choice(value_list) for i in range(int(header_count))]
+            writer.writerow(row)
 
-if delim == "tab":
-    delim = "\t"
 
-print("\nWriting test data delimited file...\n")
+if __name__ == "__main__":
 
-with open(write_path, mode="w", newline="", encoding="utf-8") as f:
-    writer = csv.writer(f, delimiter=delim)
-    header = ["c" + str(i + 1) for i in range(int(header_count))]
-    writer.writerow(header)
-    for i in range(int(rows)):
-        value_list = [rand_string(), rand_integer()]
-        row = [choice(value_list) for i in range(int(header_count))]
-        writer.writerow(row)
+    intro = """
+This application will create test data with random strings and integers of
+specified length. Columns are generically named C1, C2...Cn. Test data will
+be written in csv format to the current working directory.
+    """
 
-print(f"Test data writing completed at {home}\\{file_name}.csv\n")
+    print(intro)
+
+    time.sleep(3)
+
+    home = str(Path.home())
+    header_count = input("Enter number of columns to create: ")
+    rows = input("Enter number of rows to create: ")
+    val_len = input("Enter length for random string or integer: ")
+    delim = input("Enter delimiter. If tab, enter 'tab': ")
+    file_name = input("Enter file name: ")
+    write_path = f"{home}/{file_name}.csv"
+
+    if delim == "tab":
+        delim = "\t"
+
+    print("\nWriting test data delimited file...\n")
+
+    write_test_data()
+
+    print(f"Test data writing completed at {home}\\{file_name}.csv\n")
