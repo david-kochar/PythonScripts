4bb4ad1b68e8778b841dd8f13d18c8caf6e3dfb7
9AD589B1 <kocha017@gmail.com>
2020-10-08 17:01:34 -0500

    Update postgres_copy_from_expert.py

--- postgres_copy_from_expert.py
+++ postgres_copy_from_expert.py
@@ -1,4 +1,5 @@
 import getpass
+import os
 
 import psycopg2
 
@@ -21,48 +22,55 @@ def copy_from_local():
     if conn:
         print("\nConnection Successful!\n")
 
-    schema_name = input("Enter schema name: ")
-    table_name = input("Enter table name: ")
-    qual_table = f"{schema_name}.{table_name}"
-    header = input("Does the file have a header? (y/n): ")
-    delim = input("Enter delimiter. If tab, enter 'tab': ")
-    file_format = input("Enter 'TEXT' or 'CSV' file format: ")
-    file_path = input("Enter fully qualified file path: ")
+    file_dir = input("Enter fully qualified file path source to copy from: ")
+    header = input("Do files have a header? (y/n): ")
+    delim = input("Enter files' delimiter. If tab, enter 'tab': ")
     quote_char = input("Enter quote character: ")
     esc_char = input("Enter quote escape character: ")
+    schema_name = input("Enter target schema name: ")
+    table_name = input("Enter target table name: ")
+    qual_table = f"{schema_name}.{table_name}"
 
     if quote_char == '"':
-        quote_char = """'\\"'"""
+        quote_char = """'"'"""
     else:
         quote_char = "''''"
 
     if esc_char == '"':
-        esc_char = """'\\"'"""
+        esc_char = """'"'"""
     else:
         esc_char = "''''"
 
     if delim == "tab":
         delim = "\t"
 
-    copy_sql = f"COPY {qual_table} \
-                 FROM '{file_path}' \
-                 DELIMITER '{delim}' \
-                 {file_format} \
-                 QUOTE {quote_char} \
-                 ESCAPE {esc_char};"
-
     cur = conn.cursor()
 
-    print(f"\nCopying data into {schema_name}.{table_name}...\n")
+    print("\nCopying data...\n")
 
-    with open(file_path, "r") as f:
-        if header.upper() == "Y":
-            next(f)  # Skip the header row
-            cur.copy_expert(copy_sql, f, size=8192)
-        else:
-            cur.copy_expert(copy_sql, f, size=8192)
+    copy_list = [
+        os.path.join(file_dir, f)
+        for f in os.listdir(file_dir)
+        if f[-3:] in ["txt", "csv"]
+    ]
 
-    print("Copying data complete!\n")
+    for file_path in copy_list:
+        file_format = file_path[-3:]
+        copy_sql = f"COPY {qual_table} \
+                     FROM '{file_path}' \
+                     DELIMITER '{delim}' \
+                     {file_format} \
+                     QUOTE {quote_char} \
+                     ESCAPE {esc_char};"
+        print(f"Copying from {file_path} into {schema_name}.{table_name}")
+        with open(file_path, "r") as f:
+            if header.upper() == "Y":
+                next(f)  # Skip the header row
+                cur.copy_expert(copy_sql, f, size=8192)
+            else:
+                cur.copy_expert(copy_sql, f, size=8192)
+
+    print("\nCopying data complete!\n")
 
     conn.commit()
     cur.close()
