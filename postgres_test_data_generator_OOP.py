624aed5266941c5e1ea2c6fd5d57560dd5abbfaf
9AD589B1 <kocha017@gmail.com>
2020-10-14 15:42:46 -0500

    Update postgres_test_data_generator_OOP.py

--- postgres_test_data_generator_OOP.py
+++ postgres_test_data_generator_OOP.py
@@ -6,7 +6,7 @@ from random import choice
 
 
 class create_test_data:
-    def __init__(self, file_name, delim, column_count, row_count, val_len):
+    def __init__(self):
         self.home = str(Path.home())
         self.column_count = input("Enter number of columns to create: ")
         self.row_count = input("Enter number of rows to create: ")
@@ -18,24 +18,24 @@ class create_test_data:
         self.schema_name = input("Enter schema name for DDL: ")
         self.table_name = input("Enter table name for DDL: ")
 
-    def rand_string(self):
+    def rand_string(self, val_len):
         n = int(self.val_len)
         text = "".join(rand.choices(string.ascii_uppercase, k=n))
         return text
 
-    def rand_integer(self):
+    def rand_integer(self, val_len):
         n = int(self.val_len)
         num = rand.randint(pow(10, n - 1), pow(10, n) - 1)
         return num
 
     def write_test_data(self):
-        val_len = self.val_len
         csv_write_path = f"{self.home}/{self.file_name}.csv"
         column_count = int(self.column_count)
         header = ["C" + str(i + 1) for i in range(column_count)]
+        val_len = self.val_len
         value_list = [self.rand_string(val_len), self.rand_integer(val_len)]
         row = [choice(value_list) for i in range(column_count)]
-        with open(csv_write_path, mode="w", encoding="utf-8") as f:
+        with open(csv_write_path, mode="w", newline="", encoding="utf-8") as f:
             writer = csv.writer(f, delimiter=self.delim)
             writer.writerow(header)
             for i in range(int(self.row_count)):
@@ -45,7 +45,9 @@ class create_test_data:
         ddl_write_path = f"{self.home}/{self.table_name}.sql"
         column_count = int(self.column_count)
         header = ["C" + str(i + 1) for i in range(column_count)]
-        row = self.row
+        val_len = self.val_len
+        value_list = [self.rand_string(val_len), self.rand_integer(val_len)]
+        row = [choice(value_list) for i in range(column_count)]
         qual_table = f"{self.schema_name}.{self.table_name}"
         data_types = ["numeric" if str(i).isnumeric() else "text" for i in row]
         ddl_list = list(zip(header, data_types))
